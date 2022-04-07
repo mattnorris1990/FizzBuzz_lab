e373d1a9925545741976d3081e374dc07d62a969
mattnorris1990 <mattnorris1990@gmail.com>
2022-04-07 12:07:00 +0100

    added negative divisible by 3 test

--- fizzbuzz_test.py
+++ fizzbuzz_test.py
@@ -4,7 +4,6 @@ from fizzbuzz import *
 
 class TestFizzbuzz(unittest.TestCase):
     def setUp(self):
-        print("hello")
         self.numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 
 
@@ -13,3 +12,6 @@ class TestFizzbuzz(unittest.TestCase):
 
     def test_is_number_divisible_by_3(self):
         self.assertEqual("Fizz", fizz_buzz(self.numbers[3]))
+
+    def test_is_number_not_divisible_by_3(self):
+        self.assertEqual(4, fizz_buzz(self.numbers[4]))
